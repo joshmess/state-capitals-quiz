50f0d136d3fd627de7ee802e3835f718a510f51f
Josh <joshmessitte@gmail.com>
2021-11-08 11:21:50 -0500

    Implement Random Question Generator

--- app/src/main/java/edu/uga/cs/state_capitals_quiz/QuizActivity.java
+++ app/src/main/java/edu/uga/cs/state_capitals_quiz/QuizActivity.java
@@ -1,18 +1,41 @@
 package edu.uga.cs.state_capitals_quiz;
-
+import java.util.Random;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class QuizActivity extends AppCompatActivity {
 
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_layout);
+        text = findViewById(R.id.textView3);
+        ArrayList<Integer> questions = getQuestions();
+        String q = questions.toString();
+        text.setText(q);
 
+    }
+
+    public ArrayList<Integer> getQuestions(){
+
+        ArrayList<Integer> toReturn = new ArrayList<Integer>();
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(50) + 1;
+        toReturn.add(randomInt);
+
+        while(toReturn.size() < 6){
+            randomInt = randomGenerator.nextInt(50) + 1;
+            if(!toReturn.contains(randomInt))
+                toReturn.add(randomInt);
+        }
+        return toReturn;
     }
 
 }
